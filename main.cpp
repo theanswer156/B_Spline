0f96f06f8aa0c7a1760a0ed66e56978680140d58
TheAnswer534 <534129983@qq.com>
2024-07-10 17:36:01 +0800

    绘图示范程序  结合钉钉的图片可以用自己的数据绘制图像

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "mywindow.h"
 #include <QApplication>
-
+#include <QVBoxLayout>
+#include <QMainWindow>
+#include <QWidget>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MyWindow w;
+    QMainWindow w;
+    //这里没有指定父类  后面用的时候要以引用的形式使用
+    QWidget *centralWidget = new QWidget(&w);
+    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+
+    MyWindow *mywindow = new MyWindow();
+    mywindow->setFixedSize(1000,200);
+    layout->addWidget(mywindow);
+    w.setCentralWidget(centralWidget);
     w.show();
 
     return a.exec();
--- mywindow.h
+++ mywindow.h
@@ -1,22 +1,59 @@
 #ifndef MYWINDOW_H
 #define MYWINDOW_H
+#include <QWidget>
+#include <QPainter>
+#include <QPen>
+#include <QBrush>
+#include <QVector>
+#include <QRect>
 
-#include <QMainWindow>
-
-namespace Ui {
-class MyWindow;
-}
-
-class MyWindow : public QMainWindow
+class MyWindow : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit MyWindow(QWidget *parent = nullptr);
-    ~MyWindow();
+    explicit MyWindow(QWidget *parent = nullptr):QWidget(parent)
+    {
+        initData();
+    }
+    void initData()
+    {
+        // 随机生成一些数据点
+//        Qt默认界面的坐标原点在左上角
+        for (int i = 0; i < 1000; i+=20) {
+            dataPoints.append(QPointF(i, static_cast<float>(rand()) / RAND_MAX * 100));
+        }
+    }
+//    ~MyWindow()override;
+protected:
+    void paintEvent(QPaintEvent *event) override{
+        Q_UNUSED(event);
+        QPainter painter(this);
+        painter.setRenderHint(QPainter::Antialiasing);
+        drawLineChart(&painter);
+    }
 
 private:
-    Ui::MyWindow *ui;
+    void drawLineChart(QPainter *painter){
+        QPen pen(Qt::red);
+        pen.setWidth(1);
+        painter->translate(0,50);
+        painter->setPen(pen);
+//        绘制折线图
+        for (int i = 0; i < dataPoints.size() - 1; i++) {
+            QPointF start = dataPoints[i];
+            QPointF end = dataPoints[i + 1];
+            painter->drawLine(start, end);
+        }
+//        标记数据点
+        QBrush brush(Qt::blue);
+
+        for(const QPointF &point:dataPoints){
+            painter->setBrush(brush);
+            painter->drawEllipse(point,2,2);
+        }
+    }
+    QVector<QPointF> dataPoints;
 };
 
 #endif // MYWINDOW_H
